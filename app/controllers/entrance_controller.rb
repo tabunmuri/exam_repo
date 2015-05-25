58daaa3e4499fbbfa82e318f8f77e301485c85a6
tabun_muri <tabun.muri100@gmail.com>
2015-05-25 10:21:56 +0900

    関数にコメントと、ロジックを追加

--- app/controllers/entrance_controller.rb
+++ app/controllers/entrance_controller.rb
@@ -1,7 +1,21 @@
 class EntranceController < ApplicationController
+  #
+  # show
+  # 数字を入力する画面
+  # Author:: shunsuke.fujii
+  # Create:: 2015/05/25
+  # Copyright:: tabun_muri <tabun.muri100@gmail.com>
+  #
   def show
   end
 
+  #
+  # answer
+  # 回答を出力する画面
+  # Author:: shunsuke.fujii
+  # Create:: 2015/05/25
+  # Copyright:: tabun_muri <tabun.muri100@gmail.com>
+  #
   def answer
     p "params => #{params}"
     if params["type"] == 0
@@ -9,6 +23,7 @@ class EntranceController < ApplicationController
 
     else params["type"] == 1
       # 1〜9999のうち、抜いて欲しい数字を直接入力する場合
+      nums = Number.str_to_numbers(params["nums"])
     end
   end
 end
