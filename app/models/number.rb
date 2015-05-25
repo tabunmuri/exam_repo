50c13a3935c0b53dfed5b2b893e6c48e3bba2f0c
tabun_muri <tabun.muri100@gmail.com>
2015-05-25 10:22:29 +0900

    str_to_numbersを追加
     - カンマ区切りで数字が繋がれている文字列を数値の配列に変換する関数

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,3 +1,22 @@
 class Number < ActiveRecord::Base
+  def self.find_empty_number
 
+  end
+
+
+  def self.create_collect_numbers
+
+  end
+
+  #
+  # str_to_numbers
+  # カンマ区切りで数字がつながれた文字列から、数値の配列を作成
+  # Author:: shunsuke.fujii
+  # Create:: 2015/05/25
+  # Copyright:: tabun_muri <tabun.muri100@gmail.com>
+  #
+  def self.str_to_numbers(numberString="")
+    numbers = numberString.split(",").map {|val| val.to_i}
+    numbers.reject!{|e| e == 0}
+  end
 end
