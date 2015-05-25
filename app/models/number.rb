2c6cd66e8887380d8394d2f309f11e7539d4841e
tabun_muri <tabun.muri100@gmail.com>
2015-05-25 22:50:06 +0900

    find_empty_numberのたたき台を作成
     - 条件分岐などの、おおまかな流れを作成

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,6 +1,9 @@
 class Number < ActiveRecord::Base
   # @maxNumber = 9999
   @maxNumber = 99
+
+  # 1〜10000まで足した合計
+  @numTotalSum = 50005000
   #
   # find_empty_number
   # 配列の中から、1〜9999の中で抜けている数値を検索する為の関数
@@ -8,10 +11,17 @@ class Number < ActiveRecord::Base
   # Create:: 2015/05/25
   # Copyright:: tabun_muri <tabun.muri100@gmail.com>
   #
-  def self.find_empty_number
+  def self.find_empty_number(numberList = [])
+    # 渡された配列の個数が、0個の時は、抜け番は全部のため、自動生成する。
+    return (1..@maxNumber).to_a if numberList.size == 0
 
+    if numberList.size == 9999
+      # 渡された配列の個数が、9999個の時は、抜け数字は必ず1つに絞られる為、引き算をする
+    else
+      # それ以外の時
+
+    end
   end
-
 
   #
   # create_collect_numbers
@@ -35,8 +45,9 @@ class Number < ActiveRecord::Base
   # Create:: 2015/05/25
   # Copyright:: tabun_muri <tabun.muri100@gmail.com>
   #
-  def self.str_to_numbers(numberString="")
+  def self.str_to_numbers(numberString = "")
     numbers = numberString.split(",").map {|val| val.to_i}
     numbers.reject!{|e| e == 0}
   end
+
 end
