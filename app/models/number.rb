f2eac05b93253d3167056edfd7a93ec6969b598b
tabun_muri <tabun.muri100@gmail.com>
2015-05-28 09:53:32 +0900

    find_empty_numberのリファクタリング
     - 条件が複雑になってきた為、シンプルなものは後置ifに書き換える

--- app/models/number.rb
+++ app/models/number.rb
@@ -18,35 +18,29 @@ class Number < ActiveRecord::Base
     # 渡された配列の個数が、0個もしくはnilの時は、抜け番は全部のため、自動生成する。
     return @@numbersList if numberList.blank? || numberList.size == 0
 
-    if numberList.size == @@maxNumber
+    # 渡された配列の個数が、@@maxNumberと同値の時は、抜け数字は必ず1つに絞られる為、引き算をする
+    return @@numTotalSum - @@numTotalSum if numberList.size == @@maxNumber
 
-      # 渡された配列の個数が、9999個の時は、抜け数字は必ず1つに絞られる為、引き算をする
-      return @@numTotalSum - @@numTotalSum
-
-    else
-
-      # それ以外の時
-      if algorithmType.blank? || algorithmType == 0
-        # お手軽だけど、中は結構ヤバそうな方法
-        return (1..@@maxNumber).to_a - numberList
-      end
-
-      # 配列から与えられた配列の数値を削除する事で実現する方法
-      if algorithmType == 1
-        return create_collect_numbers(numberList)
-      end
-
-      # hash使って、引いてくる方法
-      if algorithmType == 2
-        answer = []
-        numbersHash = create_numbers_list_hash(numberList)
-        @@numbersList.each do |val|
-          answer.push(val) if !numbersHash.has_key?(val)
-        end
-        return answer
-      end
-
+    if algorithmType.blank? || algorithmType == 0
+      # お手軽だけど、中は結構ヤバそうな方法
+      return (1..@@maxNumber).to_a - numberList
     end
+
+    # 配列から与えられた配列の数値を削除する事で実現する方法
+    if algorithmType == 1
+      return create_collect_numbers(numberList)
+    end
+
+    # hash使って、引いてくる方法
+    if algorithmType == 2
+      answer      = []
+      numbersHash = create_numbers_list_hash(numberList)
+      @@numbersList.each do |val|
+        answer.push(val) if !numbersHash.has_key?(val)
+      end
+      return answer
+    end
+
   end
 
   #
