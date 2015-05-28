00a04a219d1daf0bb4c172345de39ef620447ae2
tabun_muri <tabun.muri100@gmail.com>
2015-05-28 09:31:38 +0900

    抜け番を探し出すアルゴリズムを追加

--- app/controllers/entrance_controller.rb
+++ app/controllers/entrance_controller.rb
@@ -17,13 +17,14 @@ class EntranceController < ApplicationController
   # Copyright:: tabun_muri <tabun.muri100@gmail.com>
   #
   def answer
-    p "params => #{params}"
-    if params["type"] == 0
-      # 全ての数字を入力する場合
 
-    else params["type"] == 1
-      # 1〜9999のうち、抜いて欲しい数字を直接入力する場合
-      nums = Number.create_collect_numbers(Number.str_to_numbers(params["nums"]))
+    numbersList = Number.str_to_numbers(params["nums"])
+    if params["type"] == 1
+        # 1〜9999のうち、抜いて欲しい数字を直接入力する場合は、処理をする
+      numbersList = Number.create_collect_numbers(numbersList)
     end
+    @input = numbersList
+    @answer = Number.find_empty_number(numbersList, 2)
+    p "@answer => #{@answer}"
   end
 end
--- app/models/number.rb
+++ app/models/number.rb
@@ -1,9 +1,12 @@
 class Number < ActiveRecord::Base
   # @maxNumber = 9999
-  @maxNumber = 99
+  @@maxNumber = 10
 
   # 1〜10000まで足した合計
-  @numTotalSum = 50005000
+  @@numTotalSum = 50005000
+
+  @@numbersList = (1..@@maxNumber).to_a
+
   #
   # find_empty_number
   # 配列の中から、1〜9999の中で抜けている数値を検索する為の関数
@@ -11,14 +14,37 @@ class Number < ActiveRecord::Base
   # Create:: 2015/05/25
   # Copyright:: tabun_muri <tabun.muri100@gmail.com>
   #
-  def self.find_empty_number(numberList = [])
-    # 渡された配列の個数が、0個の時は、抜け番は全部のため、自動生成する。
-    return (1..@maxNumber).to_a if numberList.size == 0
+  def self.find_empty_number(numberList = [], algorithmType = 0)
+    # 渡された配列の個数が、0個もしくはnilの時は、抜け番は全部のため、自動生成する。
+    return @@numbersList if numberList.blank? || numberList.size == 0
 
-    if numberList.size == 9999
+    if numberList.size == @@maxNumber
+
       # 渡された配列の個数が、9999個の時は、抜け数字は必ず1つに絞られる為、引き算をする
+      return @@numTotalSum - numberList.inject {|sum, n| sum + n }
+
     else
+
       # それ以外の時
+      if algorithmType.blank? || algorithmType == 0
+        # お手軽だけど、中は結構ヤバそうな方法
+        return (1..@@maxNumber).to_a - numberList
+      end
+
+      # 配列から与えられた配列の数値を削除する事で実現する方法
+      if algorithmType == 1
+        return create_collect_numbers(numberList)
+      end
+
+      # hash使って、引いてくる方法
+      if algorithmType == 2
+        answer = []
+        numbersHash = create_numbers_list_hash(numberList)
+        @@numbersList.each do |val|
+          answer.push(val) if !numbersHash.has_key?(val)
+        end
+        return answer
+      end
 
     end
   end
@@ -31,7 +57,7 @@ class Number < ActiveRecord::Base
   # Copyright:: tabun_muri <tabun.muri100@gmail.com>
   #
   def self.create_collect_numbers(emptyNumbers = [])
-    numbers = (1..@maxNumber).to_a
+    numbers = @@numbersList
     emptyNumbers.each do |val|
       numbers.reject!{|e| e == val}
     end
@@ -48,6 +74,20 @@ class Number < ActiveRecord::Base
   def self.str_to_numbers(numberString = "")
     numbers = numberString.split(",").map {|val| val.to_i}
     numbers.reject!{|e| e == 0}
+    return numbers
   end
 
+  private
+
+  def self.create_numbers_list_hash(numnersList = [])
+    numbersHash = {}
+    numnersList.each {|val| numbersHash[val] = false}
+    return numbersHash
+  end
+
+  def self.create_numbers_hash(min = 1, max = @@maxNumber)
+    numbersHash = {}
+    numnersList.each {|val| numbersHash[val] = false}
+    return numbersHash
+  end
 end
